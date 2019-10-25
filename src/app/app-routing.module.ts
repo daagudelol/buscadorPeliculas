40516c7b64a10cc892a83ae39efa7b9d8881dac0
Diego <Diego@MacBook-Air-de-Diego.local>
2019-10-24 21:26:43 -0500

    Cambio de seguridad

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from './components/home/home.component';
 import { BuscarComponent } from './components/buscar/buscar.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { FavoritosComponent } from './components/favoritos/favoritos.component';
+import { PeliculaComponent } from './components/pelicula/pelicula.component';
 
 
 const routes: Routes = [
@@ -12,6 +13,7 @@ const routes: Routes = [
   {path: 'buscar', component: BuscarComponent},
   {path: 'favoritos', component: FavoritosComponent},
   {path: 'buscar/:texto', component: BuscarComponent},
+  {path: 'pelicula/:id/:pag', component: PeliculaComponent},
   {path: '**', component: PageNotFoundComponent},
 
 ];
